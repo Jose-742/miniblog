37afb9d9c723db993182ad6c59f541d48f74e6f7
jose.bento <jose.natal@ibrowse-sds.com.br>
2025-01-21 22:54:49 -0300

    Criando o contexto da autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
+// context
+import { AuthProvider } from './context/AuthContext';
+
 // pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -19,18 +22,20 @@ function App() {
   return (
     <>
       <div className='App'>
+       <AuthProvider>
         <BrowserRouter>
-          <Navbar/>
-          <div className='container'>
-            <Routes>
-                <Route path='/' element={<Home />}/>
-                <Route path='/about' element={<About />}/>
-                <Route path='/login' element={<Login />}/>
-                <Route path='/register' element={<Register />}/>
-            </Routes>
-          </div>
-          <Footer/>
-        </BrowserRouter>
+            <Navbar/>
+            <div className='container'>
+              <Routes>
+                  <Route path='/' element={<Home />}/>
+                  <Route path='/about' element={<About />}/>
+                  <Route path='/login' element={<Login />}/>
+                  <Route path='/register' element={<Register />}/>
+              </Routes>
+            </div>
+            <Footer/>
+          </BrowserRouter>
+       </AuthProvider>
       </div>
     </>
   )
